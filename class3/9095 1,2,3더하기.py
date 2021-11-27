e705f77e73cdcffcad10db74e3f7d2cc61ce38ea
0008mari <0008mari@gmail.com>
2021-11-27 15:18:17 +0900

    new file

--- class3/9095 1,2,3더하기.py
+++ class3/9095 1,2,3더하기.py
@@ -1,1 +1,22 @@
 # 9095 1,2,3더하기
+
+
+def A(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    return A(n-1) + A(n-2) + A(n-3)
+
+
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    result = A(n)
+    print(result)
+
+
+
